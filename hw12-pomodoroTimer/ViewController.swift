bebcf4dab73653be6a5beda0e8076c0983b2c943
vincentkovalski <kovall89@gmail.com>
2022-06-02 13:58:52 +0500

    add label and button

--- hw12-pomodoroTimer/ViewController.swift
+++ hw12-pomodoroTimer/ViewController.swift
@@ -9,9 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    private let timeLabel = UILabel()
+
+    private let button = UIButton(type: .system)
+
+
+    // MARK: - Views
+
+
+
+
+    // MAKR: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        setupHierarchy()
+        setupView()
+        setupLayout()
+    }
+
+
+    private func setupHierarchy() {
+        view.addSubview(timeLabel)
+        view.addSubview(button)
+
+    }
+
+    private func setupLayout() {
+
+    }
+
+    private func setupView() {
+
+        timeLabel.font = .systemFont(ofSize: 34)
+
+        button.setImage(UIImage(systemName: "pause"), for: .selected)
+        button.setImage(UIImage(systemName: "play"), for: .normal)
+
     }
 
 
